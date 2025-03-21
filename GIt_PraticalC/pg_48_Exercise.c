60f9b3966b657f8323e0fb0a7b57ecf22464253f
abaltazapinto <andrebaltazarpinto@gmail.com>
2025-03-21 20:54:25 +0000

    Init

---
+++ GIt_PraticalC/pg_48_Exercise.c
@@ -0,0 +1,17 @@
+/* Create a stylle sheet for class assignements. Discuss what comments should go 
+ into the programs and why.*/
+/******************************************************************************************************
+I learned how to make an abbreviation then i was making inserting comments easy.
+******************************************************************************************************/
+/******************************************************************************************************
+one thing that i learned on this chapter was about the coding religions
+
+-
+
+Another thing that is important is the code Format and we need to rigorous and have a structure within and have the same identation for the same purpose.
+
+******************************************************************************************************/
+// another thing that is really important is that we have to have clarity and simplicity when writing code.
+
+
+
